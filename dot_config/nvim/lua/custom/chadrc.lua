4e6726df3f838a5beb9a4e7ba10c290beba8be59
Oscar Alberto Tovar <oscar.alberto.tovar@gmail.com>
2023-03-20 23:21:27 +0000

    Fix custom chadrc.lua

--- dot_config/nvim/lua/custom/chadrc.lua
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -1,9 +1,20 @@
--- First read our docs (completely) then check the example_config repo
-
+---@type ChadrcConfig
 local M = {}
 
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
 M.ui = {
-  theme = "dark_horizon",
+  theme = "onedark",
+  theme_toggle = { "onedark", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
 }
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
 
 return M
